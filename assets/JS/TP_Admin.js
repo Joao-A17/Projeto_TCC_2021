aec4d1e5fa5cf063e38bfaf62542c52be232f1b6
João Victor <joaovictorca2004@gmail.com>
2021-09-02 22:26:10 -0300

    Craindo_Elementos

--- assets/JS/TP_Admin.js
+++ assets/JS/TP_Admin.js
@@ -1,13 +1,41 @@
 /* Função de Adicionar a div PintCompletaa */
-var FundoPint = document.getElementById("Cont-Master");
+var ContMaster = document.getElementById("Cont-Master");
 var PintDiv = document.querySelector(".Pint-Completa");
 
 function Adicionar(){   /* 
     var Clone = PintDiv.cloneNode(true);
     FundoPint.appendChild(Clone); */
-    var NovaPintDiv = document.createElement('div');
-    FundoPint.appendChild(NovaPintDiv);
-    NovaPintDiv.className.add('Pint-Completa')
+    var NovaPintCompleta = document.createElement('div');
+    var NovaFundoPintDiv = document.createElement('div');
+    var NovaPintura = document.createElement('img');
+    var NovoFundoDecs = document.createElement('div');
+    var NovoNomePint = document.createElement('h1');
+    var NovaDescPint = document.createElement('p');
+    var NovoBtnExcluir = document.createElement('buttom');
+    var NovoBtnIcon = document.createElement('i');
+    var NovoMenuEdit = document.createElement('div');
+    var NovotxtAlter = document.createElement('h2');
+    var NovoInputNN = document.createElement('input');
+    var NovoInputDN = document.createElement('input');
+    var NovoInputFoto = document.createElement('input');
+    var NovoLabelSelect = document.createElement('label');
+    ContMaster.appendChild(NovaPintCompleta);
+    NovaPintCompleta.appendChild(NovaFundoPintDiv);
+    NovaFundoPintDiv.appendChild(NovaPintura);
+    NovaPintCompleta.appendChild(NovoFundoDecs);
+    NovoFundoDecs.appendChild(NovoNomePint);    
+    NovoFundoDecs.appendChild(NovaDescPint);    
+    NovoFundoDecs.appendChild(NovoBtnExcluir);
+    NovoBtnExcluir.appendChild(NovoBtnIcon);
+    NovaPintCompleta.className += "Pint-Completa";
+    NovaFundoPintDiv.className += "Fundo-Pint";
+    NovaPintura.src = "../assets/IMAGES/Edits/Ban.png";
+    NovaPintura.className += "Pintura0";
+    NovoFundoDecs.className += "Fundo-desc";
+    NovoNomePint.id = "Nome-Pint";
+    NovaDescPint.id = "Desc-Pint";
+    NovoBtnExcluir.className += "btn-delete";
+    NovoBtnIcon.className += "fas fa-trash-alt IconG";
 }
 /* Aparecer a div de Editar
    Pegar o butão e a div com o document
@@ -17,12 +45,20 @@ function Adicionar(){   /*
    e assim sucessivamente */
 var btnEdit = document.getElementById("btn-edit");
 var menuEdit = document.querySelector(".Editar");
+var btnSalvar = document.querySelector(".btn-SP");
+var btnExcluir = document.querySelector(".btn-delete");
 menuEdit.style.display = "none";
+btnSalvar.style.display = "none";
+btnExcluir.style.display = "none";
 function AparecerE(){
-    if(menuEdit.style.display === "none"){
+    if(menuEdit.style.display === "none", btnSalvar.style.display === "none",btnExcluir.style.display === "none"){
         menuEdit.style.display = "block";
+        btnSalvar.style.display = "block";
+        btnExcluir.style.display = "block";
     } else {
         menuEdit.style.display = "none";
+        btnSalvar.style.display = "none";
+        btnExcluir.style.display = "none";
     }
 }
 /* Editar Pintura
