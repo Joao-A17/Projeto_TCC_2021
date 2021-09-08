1ec489601289f9cc6fdcb77ce40d767340c4e5e4
João Victor <joaovictorca2004@gmail.com>
2021-09-08 00:07:19 -0300

    Tela de perfil admin

--- assets/JS/TP_Admin.js
+++ assets/JS/TP_Admin.js
@@ -10,12 +10,14 @@ var BtnAdd = document.getElementById("btn-add");
 var btnExcluir = document.querySelector(".btn-delete");
 var menuEdit = document.querySelector(".Editar");
 var btnSalvar = document.querySelector(".btn-SP");
+var AlteraImgPerfil = document.querySelector(".AlteraImg-Perfil");
 var BtnEditarPerfil = document.querySelector(".edp");
 var MenuEditPerfil = document.getElementById("Menu-EditPerfil");
 var BtnAparecer = document.getElementById("Aparecer");
 var BtnSPerfil = document.getElementById("BtnSP");
 var NomeAdmin = document.getElementById("Nome-Admin");
 var DescAdmin = document.getElementById("Desc-Admin");
+var InputP = document.querySelectorAll(".InputP");
 var InputNomeAdmin = document.getElementById("InputNomeAdmin");
 var InputDescAdmin = document.getElementById("InputDescAdmin");
 menuEdit.style.display = "none";
@@ -60,12 +62,14 @@ BtnAdd.addEventListener('click', function(){
     var aDescPint = document.createElement('p');
     var aBtnExcluir = document.createElement('buttom');
     var aBtnIcon = document.createElement('i');
-    var aMenuEdit = document.createElement('div');
+    var aMenuEdit = document.createElement('form');
     var atxtAlter = document.createElement('h2');
     var aInputNN = document.createElement('input');
     var aInputDN = document.createElement('input');
     var aInputFoto = document.createElement('input');
+    var aDivSelecionarPublicar = document.createElement('div');
     var aLabelSelect = document.createElement('label');
+    var aInputPublicar = document.createElement('input');
     var Espaço1 = document.createElement('div');
     ContMaster.appendChild(aPintCompleta);
     aPintCompleta.appendChild(aFundoPintDiv);
@@ -74,8 +78,8 @@ BtnAdd.addEventListener('click', function(){
     aFundoDecs.appendChild(aNomePint);    
     aFundoDecs.appendChild(aDescPint);    
     aFundoDecs.appendChild(aBtnExcluir);
-    aNomePint.innerHTML = "Nome da Pintura";
-    aDescPint.innerHTML = "Descrição da Pintura";
+    aNomePint.innerHTML = "Nome da publicação";
+    aDescPint.innerHTML = "Descrição da publicação";
     aBtnExcluir.innerHTML = "Excluir";
     aBtnExcluir.appendChild(aBtnIcon);
     aFundoDecs.appendChild(aMenuEdit);
@@ -84,7 +88,9 @@ BtnAdd.addEventListener('click', function(){
     aMenuEdit.appendChild(aInputNN);
     aMenuEdit.appendChild(aInputDN);
     aMenuEdit.appendChild(aInputFoto);
-    aMenuEdit.appendChild(aLabelSelect);
+    aMenuEdit.appendChild(aDivSelecionarPublicar);
+    aDivSelecionarPublicar.appendChild(aLabelSelect);
+    aDivSelecionarPublicar.appendChild(aInputPublicar);
     ContMaster.appendChild(Espaço1);
     aPintCompleta.className = "Pint-Completa";
     aFundoPintDiv.className = "Fundo-Pint";
@@ -96,6 +102,9 @@ BtnAdd.addEventListener('click', function(){
     aBtnExcluir.className = "btn-delete";
     aBtnExcluir.style.display = "none";
     aBtnIcon.className = "fas fa-trash-alt IconG";
+    aMenuEdit.action = "Pinturas_AP.php";
+    aMenuEdit.method = "POST";
+    aMenuEdit.enctype = "multipart/form-data";
     aMenuEdit.className = "Editar";
     aMenuEdit.style.display = "none";
     aInputNN.type = "text";
@@ -110,9 +119,15 @@ BtnAdd.addEventListener('click', function(){
     aInputFoto.name = "foto";
     aInputFoto.id = "SelectIMG0";
     aInputFoto.accept = "image/*";
+    aDivSelecionarPublicar.id = "BTN-IN";
     aLabelSelect.innerHTML ="Selecionar Imagem";
     aLabelSelect.htmlFor = "SelectIMG0";
     aLabelSelect.className = "SelectP";
+    aInputPublicar.type = "submit";
+    aInputPublicar.name = "SalvarTudo";
+    aInputPublicar.value = "Publicar";
+    aInputPublicar.className = "btn-SP";
+    aInputPublicar.onclick = "NPintura()";
     Espaço1.className = "espaço";
     /* Botão de excluir */    
     aBtnExcluir.addEventListener('click', function() {                
@@ -177,17 +192,43 @@ BtnSPerfil.addEventListener('click', function(){
     NomeAdmin.innerHTML = NomeNovo;
     DecsNova = InputDescAdmin.value;
     DescAdmin.innerHTML = DecsNova;
-    InputNomeAdmin.onreset();
 })
-/* Função de aparecer os menus de editar */
+/* Função de aparecer os menus de editar */      
+AlteraImgPerfil.style.display = "none";
+BtnSPerfil.style.display = "none";
 BtnEditarPerfil.addEventListener('click', function(){
-   if(MenuEditPerfil.style.display == "none"){
-    MenuEditPerfil.style.display = "block";
+   if(MenuEditPerfil.style.display == "none"){       
+       AlteraImgPerfil.style.display = "block";
+       MenuEditPerfil.style.display = "block";
+       BtnSPerfil.style.display = "block";
    } 
    else{
-    MenuEditPerfil.style.display = "none";
+    MenuEditPerfil.style.display = "none";    
+    AlteraImgPerfil.style.display = "none";
+    BtnSPerfil.style.display = "none";
    }
 })
+
+/*  Mudar a imagem de perfil pegando o documento da imagem e do input tipo file,    criar um EventListener change(Alterar)      */
+var BtnAlterarIMGPerfil = document.getElementById("imgPerfil");
+var ImgPerfil = document.querySelector(".Foto-Admin");
+   
+BtnAlterarIMGPerfil.addEventListener('change', function(ea) {
+    showThumbnail(this.files); /* mostrar miniatura desse mesmo arquivo */
+});
+   
+function showThumbnail(files) {
+    if (files && files[0]) {
+    var procurar = new FileReader();
+   
+    procurar.onload = function(ea) {
+        ImgPerfil.src = ea.target.result;
+    }
+   
+    procurar.readAsDataURL(files[0]);
+    }
+} 
+
 function AparecerE() { 
     if(menuEdit.style.display == "none"){
         menuEdit.style.display = "block";
@@ -199,28 +240,7 @@ function AparecerE() {
         btnExcluir.style.display = "none";
     }
 }
-/* Selecionar uma pintura nova */
-/* Pegar os document da pintura nova e antiga,
-   criar um EventListener change(Alterar)      */
 
-NovaPintura0 = document.getElementById("SelectIMG0");
-aPintura = document.querySelector(".Pintura");
-   
-NovaPintura0.addEventListener('change', function(e) {
-    showThumbnail(this.files); /* mostrar miniatura desse mesmo arquivo */
-});
-   
-function showThumbnail(files) {
-    if (files && files[0]) {
-    var procurar0 = new FileReader();
-   
-    procurar0.onload = function(e) {
-        aPintura.src = e.target.result;
-    }
-   
-    procurar0.readAsDataURL(files[0]);
-    }
-} 
 /* Botão de excluir */
 btnExcluir.addEventListener('click', function() {        
     if (PintDiv.parentNode) {
