e2cd481a15a8c01c308c5150e22aec87d33e6b33
Joao <joaovictorca2004@gmail.com>
2021-10-14 10:50:48 -0300

    Altera o perfil completo erro no alterar usuario

--- assets/PHP/Alterar_Perfil.php
+++ assets/PHP/Alterar_Perfil.php
@@ -1,8 +1,9 @@
 <?php
+
 include_once('./Conexao_Banco.php'); 
 require './dados_perfil.php';
 
-if(isset($_POST['btn_salvar'])){
+if(isset($_POST['btn_salvar'])){ 
     $usuario_logado = $_SESSION['nome-user'];   
     $Nome = filter_input(INPUT_POST, 'nome_usuario', FILTER_SANITIZE_STRING);
     $Sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_STRING);
@@ -18,17 +19,23 @@ if(isset($_POST['btn_salvar'])){
     echo "Nome: ", $Nome, "<br>";
     echo "Sobrenome: ", $Sobrenome, "<br>";
     echo "Email: ", $Email, "<br>";
-    echo "Senha: ", $Senha, "<br>";
+    echo "Senha: ", $Senha, "<br>"; 
+    echo "Descrição: ", $Desc, "<br>";
     echo "Insta: ", $Insta, "<br>";
     echo "Face: ", $Face, "<br>";
     echo "Twit: ", $Twit, "<br>";
     echo "Whats: ", $Whats, "<br>";
+    echo "Id_Usuario: ", $IdUser, "<br>"; 
+    echo "Id_Perfil: ", $Id_perfil, "<br>"; 
 
-    $UP_perfil = "UPDATE perfil SET nomep='$Nome' WHERE id=$Id_perfil";
-    if($UP_perfil){
-        echo "<h1 style='color: green;'>Certo</h1>";
+    $UP_perfil = "UPDATE perfil SET nomep = '$Nome', descp = '$Desc', instagramp = '$Insta', facebookp = '$Face', twitterp = '$Twit', telefonep = '$Whats' WHERE idperfil='$Id_perfil'";
+    $sql_perfil = mysqli_query($conexao, $UP_perfil);
+    $UP_usuarios = "UPDATE usuarios SET nome = '$Nome', sobrenome = '$Sobrenome', email = '$Email', password = '$Senha', telefone = '$Whats' WHERE id='$IdUser'";    
+    $sql_usuarios = mysqli_query($conexao, $UP_usuarios);
+    if($sql_usuarios){
+        echo "<h1 style='color: green;'>Enviado</h1>";
     }else{
-        echo "<h1 style='color: red;'>Errado</h1>";
+        echo "<h1 style='color: red;'>Não enviado</h1>";
     }
 
 }else{
